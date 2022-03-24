9faa5ba50b094d1fd3453ca175ab07c3a4d62253
mmangalg <102246141+mmangalg@users.noreply.github.com>
2022-03-24 22:29:37 +0530

    Create cloudformation-stacks.sh

---
+++ cloudformation-stacks.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+
+aws cloudformation update-stack --stack-name myteststack3 --template-body file://dynamo-cf-template.tf \
+--parameters ParameterKey=HashKeyElementName,ParameterValue=$HashKeyElementName \
+             ParameterKey=HashKeyElementType,ParameterValue=$HashKeyElementType ParameterKey=ReadCapacityUnits,ParameterValue=$ReadCapacityUnits \
+             ParameterKey=WriteCapacityUnits,ParameterValue=$WriteCapacityUnits --region us-east-2
