5917be1fde02f085fe8448bc2c15e6980752b8ef
mmangalg <102246141+mmangalg@users.noreply.github.com>
2022-03-25 01:36:27 +0530

    Update cloudformation-stacks.sh

--- cloudformation-stacks.sh
+++ cloudformation-stacks.sh
@@ -4,14 +4,14 @@ aws cloudformation wait  stack-exists --stack-name $STACK_NAME --region us-east-
 
 if [ $? -eq 0 ]
 then
-          aws cloudformation update-stack --stack-name $STACK_NAME --template-body file://dynamo-cf-template.tf \
-             --parameters ParameterKey=HashKeyElementName,ParameterValue=$HashKeyElementName \
-             ParameterKey=HashKeyElementType,ParameterValue=$HashKeyElementType ParameterKey=ReadCapacityUnits,ParameterValue=$ReadCapacityUnits \
-             ParameterKey=WriteCapacityUnits,ParameterValue=$WriteCapacityUnits --region us-east-2
+          aws cloudformation update-stack --stack-name $STACK_NAME --template-body file://dynamo-cf-template.tf 
+           #  --parameters ParameterKey=HashKeyElementName,ParameterValue=$HashKeyElementName \
+           # ParameterKey=HashKeyElementType,ParameterValue=$HashKeyElementType ParameterKey=ReadCapacityUnits,ParameterValue=$ReadCapacityUnits \
+           #  ParameterKey=WriteCapacityUnits,ParameterValue=$WriteCapacityUnits --region us-east-2
 
 else
-          aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://dynamo-cf-template.tf \
-             --parameters ParameterKey=HashKeyElementName,ParameterValue=$HashKeyElementName \
-             ParameterKey=HashKeyElementType,ParameterValue=$HashKeyElementType ParameterKey=ReadCapacityUnits,ParameterValue=$ReadCapacityUnits \
-             ParameterKey=WriteCapacityUnits,ParameterValue=$WriteCapacityUnits --region us-east-2
+          aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://dynamo-cf-template.tf 
+            # --parameters ParameterKey=HashKeyElementName,ParameterValue=$HashKeyElementName \
+            # ParameterKey=HashKeyElementType,ParameterValue=$HashKeyElementType ParameterKey=ReadCapacityUnits,ParameterValue=$ReadCapacityUnits \
+            # ParameterKey=WriteCapacityUnits,ParameterValue=$WriteCapacityUnits --region us-east-2
 fi 
