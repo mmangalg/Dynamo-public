7cd294714b46c135b2036ada8ac35a3a2d88ecb9
mmangalg <102246141+mmangalg@users.noreply.github.com>
2022-03-25 01:37:53 +0530

    Update cloudformation-stacks.sh

--- cloudformation-stacks.sh
+++ cloudformation-stacks.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-aws cloudformation wait  stack-exists --stack-name $STACK_NAME --region us-east-2
+aws cloudformation wait  stack-exists --stack-name DynamoDB --region us-east-2
 
 if [ $? -eq 0 ]
 then
-          aws cloudformation update-stack --stack-name $STACK_NAME --template-body file://dynamo-cf-template.tf 
+          aws cloudformation update-stack --stack-name DynamoDB --template-body file://dynamo-cf-template.tf --region us-east-2
            #  --parameters ParameterKey=HashKeyElementName,ParameterValue=$HashKeyElementName \
            # ParameterKey=HashKeyElementType,ParameterValue=$HashKeyElementType ParameterKey=ReadCapacityUnits,ParameterValue=$ReadCapacityUnits \
            #  ParameterKey=WriteCapacityUnits,ParameterValue=$WriteCapacityUnits --region us-east-2
 
 else
-          aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://dynamo-cf-template.tf 
+          aws cloudformation create-stack --stack-name DynamoDB --template-body file://dynamo-cf-template.tf --region us-east-2
             # --parameters ParameterKey=HashKeyElementName,ParameterValue=$HashKeyElementName \
             # ParameterKey=HashKeyElementType,ParameterValue=$HashKeyElementType ParameterKey=ReadCapacityUnits,ParameterValue=$ReadCapacityUnits \
             # ParameterKey=WriteCapacityUnits,ParameterValue=$WriteCapacityUnits --region us-east-2
