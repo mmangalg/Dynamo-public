26ab336fc85a4aa12ce6abdafc3438b3baeaad6f
mmangalg <102246141+mmangalg@users.noreply.github.com>
2022-03-24 23:18:26 +0530

    Update cloudformation-stacks.sh

--- cloudformation-stacks.sh
+++ cloudformation-stacks.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-aws cloudformation create-stack --stack-name myteststack3 --template-body file://dynamo-cf-template.tf \
---parameters ParameterKey=HashKeyElementName,ParameterValue=$HashKeyElementName \
+aws cloudformation wait  stack-exists --stack-name $STACK_NAME
+
+if [$? -eq 0]
+then
+          aws cloudformation update-stack --stack-name $STACK_NAME --template-body file://dynamo-cf-template.tf \
+             --parameters ParameterKey=HashKeyElementName,ParameterValue=$HashKeyElementName \
              ParameterKey=HashKeyElementType,ParameterValue=$HashKeyElementType ParameterKey=ReadCapacityUnits,ParameterValue=$ReadCapacityUnits \
              ParameterKey=WriteCapacityUnits,ParameterValue=$WriteCapacityUnits --region us-east-2
+
+else
+          aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://dynamo-cf-template.tf \
+             --parameters ParameterKey=HashKeyElementName,ParameterValue=$HashKeyElementName \
+             ParameterKey=HashKeyElementType,ParameterValue=$HashKeyElementType ParameterKey=ReadCapacityUnits,ParameterValue=$ReadCapacityUnits \
+             ParameterKey=WriteCapacityUnits,ParameterValue=$WriteCapacityUnits --region us-east-2
+fi 
