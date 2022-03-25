a87b0de1e283114a28db78d715facabc8c8bd908
mmangalg <102246141+mmangalg@users.noreply.github.com>
2022-03-25 11:15:02 +0530

    Update cloudformation-stacks.sh

--- cloudformation-stacks.sh
+++ cloudformation-stacks.sh
@@ -5,13 +5,11 @@ aws cloudformation wait  stack-exists --stack-name DynamoDB --region us-east-2
 if [ $? -eq 0 ]
 then
           aws cloudformation update-stack --stack-name DynamoDB --template-body file://dynamo-cf-template.tf --region us-east-2
-           #  --parameters ParameterKey=HashKeyElementName,ParameterValue=$HashKeyElementName \
-           # ParameterKey=HashKeyElementType,ParameterValue=$HashKeyElementType ParameterKey=ReadCapacityUnits,ParameterValue=$ReadCapacityUnits \
-           #  ParameterKey=WriteCapacityUnits,ParameterValue=$WriteCapacityUnits --region us-east-2
+           --parameters ParameterKey=HashKeyElementName,ParameterValue=$HashKeyElementName \
+            ParameterKey=HashKeyElementType,ParameterValue=$HashKeyElementType ParameterKey=EnvironmentName,ParameterValue=$EnvironmentName \
 
 else
           aws cloudformation create-stack --stack-name DynamoDB --template-body file://dynamo-cf-template.tf --region us-east-2
-            # --parameters ParameterKey=HashKeyElementName,ParameterValue=$HashKeyElementName \
-            # ParameterKey=HashKeyElementType,ParameterValue=$HashKeyElementType ParameterKey=ReadCapacityUnits,ParameterValue=$ReadCapacityUnits \
-            # ParameterKey=WriteCapacityUnits,ParameterValue=$WriteCapacityUnits --region us-east-2
+            --parameters ParameterKey=HashKeyElementName,ParameterValue=$HashKeyElementName \
+             ParameterKey=HashKeyElementType,ParameterValue=$HashKeyElementType ParameterKey=EnvironmentName,ParameterValue=$EnvironmentName \
 fi 
