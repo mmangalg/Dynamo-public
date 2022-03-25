0573e3d1e0bea2784cdf4cc5903ded57e59a508b
mmangalg <102246141+mmangalg@users.noreply.github.com>
2022-03-25 17:03:36 +0530

    Update cloudformation-stacks.sh

--- cloudformation-stacks.sh
+++ cloudformation-stacks.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-aws cloudformation wait  stack-exists --stack-name DynamoDB --region us-east-2
+aws cloudformation wait  stack-exists --stack-name $StackName --region $Region
 
 if [ $? -eq 0 ]
 then
-          aws cloudformation update-stack --stack-name DynamoDB --template-body file://dynamo-cf-template.tf --region us-east-2 \
+          aws cloudformation update-stack --stack-name StackName --template-body file://dynamo-cf-template.tf --region $Region \
            --parameters ParameterKey=HashKeyElementName,ParameterValue=$HashKeyElementName \
             ParameterKey=HashKeyElementType,ParameterValue=$HashKeyElementType ParameterKey=EnvironmentName,ParameterValue=$EnvironmentName 
 
 else
-          aws cloudformation create-stack --stack-name DynamoDB --template-body file://dynamo-cf-template.tf --region us-east-2 \
+          aws cloudformation create-stack --stack-name StackName --template-body file://dynamo-cf-template.tf --region $Region \
             --parameters ParameterKey=HashKeyElementName,ParameterValue=$HashKeyElementName \
              ParameterKey=HashKeyElementType,ParameterValue=$HashKeyElementType ParameterKey=EnvironmentName,ParameterValue=$EnvironmentName
 fi 
