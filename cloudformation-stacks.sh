5846d603d4b3416f3f3f846cf4a3c4a5e7e2ff06
mmangalg <102246141+mmangalg@users.noreply.github.com>
2022-03-28 22:23:58 +0530

    Update cloudformation-stacks.sh

--- cloudformation-stacks.sh
+++ cloudformation-stacks.sh
@@ -8,14 +8,14 @@ aws cloudformation wait  stack-exists --stack-name $StackName --region $Region
 if [ $? -eq 0 ]
 then
 
-          aws cloudformation update-stack --stack-name $StackName --template-body file://dynamo-cf-template.tf --region $Region \
+          aws cloudformation update-stack --stack-name $StackName --template-body file://dynamo-cf-template.yaml --region $Region \
            --parameters ParameterKey=PrimaryKeyName,ParameterValue=$PrimaryKeyName \
             ParameterKey=PrimaryKeyType,ParameterValue=$PrimaryKeyType ParameterKey=EnvironmentName,ParameterValue=$EnvironmentName \
             ParameterKey=Region,ParameterValue=$Region
 
 else
 
-          aws cloudformation create-stack --stack-name $StackName --template-body file://dynamo-cf-template.tf --region $Region \
+          aws cloudformation create-stack --stack-name $StackName --template-body file://dynamo-cf-template.yaml --region $Region \
             --parameters ParameterKey=PrimaryKeyName,ParameterValue=$PrimaryKeyName \
              ParameterKey=PrimaryKeyType,ParameterValue=$PrimaryKeyType ParameterKey=EnvironmentName,ParameterValue=$EnvironmentName\
              ParameterKey=Region,ParameterValue=$Region
