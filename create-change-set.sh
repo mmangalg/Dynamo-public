89402c61ffd14247c70d70e1439289ff624ce0f3
mmangalg <102246141+mmangalg@users.noreply.github.com>
2022-03-28 23:16:20 +0530

    Update create-change-set.sh

--- create-change-set.sh
+++ create-change-set.sh
@@ -8,15 +8,18 @@ aws cloudformation wait  stack-exists --stack-name $StackName --region $Region
 if [ $? -eq 0 ]
 then
           echo "creating change set for existing stack "
-          aws cloudformation create-change-set     --stack-name DynamoDBStack-Staging     --change-set-name my-change-set  \
+          OUT=$(aws cloudformation create-change-set     --stack-name DynamoDBStack-Staging     --change-set-name my-change-set  \
               --template-body file://dynamo-cf-template.yaml --region us-west-2  --parameters ParameterKey=PrimaryKeyName,ParameterValue=Name   \
-                ParameterKey=PrimaryKeyType,ParameterValue=S ParameterKey=EnvironmentName,ParameterValue=Staging ParameterKey=Region,ParameterValue=us-west-2 --change-set-type UPDATE
+                ParameterKey=PrimaryKeyType,ParameterValue=S ParameterKey=EnvironmentName,ParameterValue=Staging ParameterKey=Region,ParameterValue=us-west-2 --change-set-type UPDATE)
 
 else
       
           echo "creating change set for new stack"
-          aws cloudformation create-change-set     --stack-name DynamoDBStack-Staging     --change-set-name my-change-set  \
+          OUT=$(aws cloudformation create-change-set     --stack-name DynamoDBStack-Staging     --change-set-name my-change-set  \
               --template-body file://dynamo-cf-template.yaml --region us-west-2  --parameters ParameterKey=PrimaryKeyName,ParameterValue=Name   \
-              ParameterKey=PrimaryKeyType,ParameterValue=S ParameterKey=EnvironmentName,ParameterValue=Staging ParameterKey=Region,ParameterValue=us-west-2 --change-set-type CREATE
+              ParameterKey=PrimaryKeyType,ParameterValue=S ParameterKey=EnvironmentName,ParameterValue=Staging ParameterKey=Region,ParameterValue=us-west-2 --change-set-type CREATE)
 
 fi 
+
+ARN=$(echo $OUT | jq -r '.Id')
+echo "Printing ARN $ARN"
