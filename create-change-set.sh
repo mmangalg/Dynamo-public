a8b8320366ba770aa09a3babb6a809b158205294
mmangalg <102246141+mmangalg@users.noreply.github.com>
2022-03-29 01:13:36 +0530

    Update create-change-set.sh

--- create-change-set.sh
+++ create-change-set.sh
@@ -12,6 +12,7 @@ then
               --template-body file://dynamo-cf-template.yaml --region $Region  --parameters ParameterKey=PrimaryKeyName,ParameterValue=$PrimaryKeyName   \
                 ParameterKey=PrimaryKeyType,ParameterValue=$PrimaryKeyType ParameterKey=EnvironmentName,ParameterValue=$EnvironmentName ParameterKey=Region,ParameterValue=$Region --change-set-type UPDATE)
           ARN=$(echo $OUT | jq -r '.Id')
+          echo "printing change set ARN: $ARN"
           sleep 15
           aws cloudformation execute-change-set --change-set-name $ARN --region $Region
 
@@ -23,6 +24,8 @@ else
               --template-body file://dynamo-cf-template.yaml --region $Region  --parameters ParameterKey=PrimaryKeyName,ParameterValue=$PrimaryKeyName   \
               ParameterKey=PrimaryKeyType,ParameterValue=$PrimaryKeyType ParameterKey=EnvironmentName,ParameterValue=$EnvironmentName ParameterKey=Region,ParameterValue=$Region --change-set-type CREATE)
           ARN=$(echo $OUT | jq -r '.Id')
+          echo "printing change set ARN: $ARN"
+
           sleep 15
           aws cloudformation execute-change-set --change-set-name $ARN --region $Region
 
