30a26cb1770d840092a3a3ce78ec6375e2c69dfe
mmangalg <102246141+mmangalg@users.noreply.github.com>
2022-03-28 21:29:19 +0530

    Create create-change-set.sh

---
+++ create-change-set.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -xe
+
+
+StackName="DynamoDBStack-${EnvironmentName}"
+
+aws cloudformation wait  stack-exists --stack-name $StackName --region $Region
+
+if [ $? -eq 0 ]
+then
+          aws cloudformation create-change-set     --stack-name DynamoDBStack-Staging     --change-set-name my-change-set  \
+              --template-body file://test.yaml --region us-west-2  --parameters ParameterKey=PrimaryKeyName,ParameterValue=Name   \
+                ParameterKey=PrimaryKeyType,ParameterValue=S ParameterKey=EnvironmentName,ParameterValue=Staging ParameterKey=Region,ParameterValue=us-west-2 --change-set-type UPDATE
+
+else
+
+          aws cloudformation create-change-set     --stack-name DynamoDBStack-Staging     --change-set-name my-change-set  \
+              --template-body file://test.yaml --region us-west-2  --parameters ParameterKey=PrimaryKeyName,ParameterValue=Name   \
+              ParameterKey=PrimaryKeyType,ParameterValue=S ParameterKey=EnvironmentName,ParameterValue=Staging ParameterKey=Region,ParameterValue=us-west-2 --change-set-type UPDATE
+
+fi 
