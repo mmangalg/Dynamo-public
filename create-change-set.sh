21cde5b9181fa5c38ef1fdb8e31b26022168fbe5
mmangalg <102246141+mmangalg@users.noreply.github.com>
2022-03-28 22:16:32 +0530

    Update create-change-set.sh

--- create-change-set.sh
+++ create-change-set.sh
@@ -7,14 +7,16 @@ aws cloudformation wait  stack-exists --stack-name $StackName --region $Region
 
 if [ $? -eq 0 ]
 then
+          echo "creating change set for existing stack "
           aws cloudformation create-change-set     --stack-name DynamoDBStack-Staging     --change-set-name my-change-set  \
               --template-body file://dynamo-cf-template.tf --region us-west-2  --parameters ParameterKey=PrimaryKeyName,ParameterValue=Name   \
                 ParameterKey=PrimaryKeyType,ParameterValue=S ParameterKey=EnvironmentName,ParameterValue=Staging ParameterKey=Region,ParameterValue=us-west-2 --change-set-type UPDATE
 
 else
-
+      
+          echo "creating change set for new stack"
           aws cloudformation create-change-set     --stack-name DynamoDBStack-Staging     --change-set-name my-change-set  \
               --template-body file://dynamo-cf-template.tf --region us-west-2  --parameters ParameterKey=PrimaryKeyName,ParameterValue=Name   \
-              ParameterKey=PrimaryKeyType,ParameterValue=S ParameterKey=EnvironmentName,ParameterValue=Staging ParameterKey=Region,ParameterValue=us-west-2 --change-set-type UPDATE
+              ParameterKey=PrimaryKeyType,ParameterValue=S ParameterKey=EnvironmentName,ParameterValue=Staging ParameterKey=Region,ParameterValue=us-west-2 --change-set-type CREATE
 
 fi 
