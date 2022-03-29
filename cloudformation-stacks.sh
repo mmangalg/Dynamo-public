bbfa62d8dca5e440c0d62bf079d1ff7263e316c9
mmangalg <102246141+mmangalg@users.noreply.github.com>
2022-03-29 13:35:56 +0530

    Update cloudformation-stacks.sh

--- cloudformation-stacks.sh
+++ cloudformation-stacks.sh
@@ -3,7 +3,4 @@
 StackName="DynamoDBStack-${EnvironmentName}"
 
 aws cloudformation deploy --stack-name $StackName --template-body file://dynamo-cf-template.yaml --region $Region  \
---parameters ParameterKey=PrimaryKeyName,ParameterValue=$PrimaryKeyName \
-  ParameterKey=PrimaryKeyType,ParameterValue=$PrimaryKeyType ParameterKey=EnvironmentName,ParameterValue=$EnvironmentName  \
-  ParameterKey=Region,ParameterValue=$Region
-
+--parameter-overrides PrimaryKeyName=$PrimaryKeyName PrimaryKeyType=$PrimaryKeyType EnvironmentName=$EnvironmentName Region=$Region
