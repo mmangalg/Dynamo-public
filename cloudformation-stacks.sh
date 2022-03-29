83e9f7bdb860e03ef4d22a31517b6655567e0132
mmangalg <102246141+mmangalg@users.noreply.github.com>
2022-03-29 13:18:28 +0530

    Update cloudformation-stacks.sh

--- cloudformation-stacks.sh
+++ cloudformation-stacks.sh
@@ -1,23 +1,9 @@
 #!/bin/bash -xe
 
-aws cloudformation execute-change-set --change-set-name arn
+StackName="DynamoDBStack-${EnvironmentName}"
 
-# StackName="DynamoDBStack-${EnvironmentName}"
+aws cloudformation deploy --stack-name $StackName --template-body file://dynamo-cf-template.yaml --region $Region  \
+--parameters ParameterKey=PrimaryKeyName,ParameterValue=$PrimaryKeyName \
+  ParameterKey=PrimaryKeyType,ParameterValue=$PrimaryKeyType ParameterKey=EnvironmentName,ParameterValue=$EnvironmentName  \
+  ParameterKey=Region,ParameterValue=$Region
 
-# aws cloudformation wait  stack-exists --stack-name $StackName --region $Region
-
-# if [ $? -eq 0 ]
-# then
-
-#           aws cloudformation update-stack --stack-name $StackName --template-body file://dynamo-cf-template.yaml --region $Region \
-#            --parameters ParameterKey=PrimaryKeyName,ParameterValue=$PrimaryKeyName \
-#             ParameterKey=PrimaryKeyType,ParameterValue=$PrimaryKeyType ParameterKey=EnvironmentName,ParameterValue=$EnvironmentName \
-#             ParameterKey=Region,ParameterValue=$Region
-
-# else
-
-#           aws cloudformation create-stack --stack-name $StackName --template-body file://dynamo-cf-template.yaml --region $Region \
-#             --parameters ParameterKey=PrimaryKeyName,ParameterValue=$PrimaryKeyName \
-#              ParameterKey=PrimaryKeyType,ParameterValue=$PrimaryKeyType ParameterKey=EnvironmentName,ParameterValue=$EnvironmentName\
-#              ParameterKey=Region,ParameterValue=$Region
-# fi 
