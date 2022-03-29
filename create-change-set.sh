012cdf735b677a152ece2ff98245c79f1dae5761
mmangalg <102246141+mmangalg@users.noreply.github.com>
2022-03-29 13:58:53 +0530

    Update create-change-set.sh

--- create-change-set.sh
+++ create-change-set.sh
@@ -3,29 +3,32 @@
 
 StackName="DynamoDBStack-${EnvironmentName}"
 
-aws cloudformation wait  stack-exists --stack-name $StackName --region $Region
+# aws cloudformation wait  stack-exists --stack-name $StackName --region $Region
 
-if [ $? -eq 0 ]
-then
-          echo "creating change set for existing stack "
-          OUT=$(aws cloudformation create-change-set     --stack-name $StackName     --change-set-name my-change-set  \
-              --template-body file://dynamo-cf-template.yaml --region $Region  --parameters ParameterKey=PrimaryKeyName,ParameterValue=$PrimaryKeyName   \
-                ParameterKey=PrimaryKeyType,ParameterValue=$PrimaryKeyType ParameterKey=EnvironmentName,ParameterValue=$EnvironmentName ParameterKey=Region,ParameterValue=$Region --change-set-type UPDATE)
-          ARN=$(echo $OUT | jq -r '.Id')
-          echo "printing change set ARN: $ARN"
-          // sleep 15
-          // aws cloudformation execute-change-set --change-set-name $ARN --region $Region
+# if [ $? -eq 0 ]
+# then
+#           echo "creating change set for existing stack "
+#           OUT=$(aws cloudformation create-change-set     --stack-name $StackName     --change-set-name my-change-set  \
+#               --template-body file://dynamo-cf-template.yaml --region $Region  --parameters ParameterKey=PrimaryKeyName,ParameterValue=$PrimaryKeyName   \
+#                 ParameterKey=PrimaryKeyType,ParameterValue=$PrimaryKeyType ParameterKey=EnvironmentName,ParameterValue=$EnvironmentName ParameterKey=Region,ParameterValue=$Region --change-set-type UPDATE)
+#           ARN=$(echo $OUT | jq -r '.Id')
+#           echo "printing change set ARN: $ARN"
+#           // sleep 15
+#           // aws cloudformation execute-change-set --change-set-name $ARN --region $Region
 
 
-else
+# else
       
-          echo "creating change set for new stack"
-          OUT=$(aws cloudformation create-change-set     --stack-name $StackName     --change-set-name my-change-set  \
-              --template-body file://dynamo-cf-template.yaml --region $Region  --parameters ParameterKey=PrimaryKeyName,ParameterValue=$PrimaryKeyName   \
-              ParameterKey=PrimaryKeyType,ParameterValue=$PrimaryKeyType ParameterKey=EnvironmentName,ParameterValue=$EnvironmentName ParameterKey=Region,ParameterValue=$Region --change-set-type CREATE)
-          ARN=$(echo $OUT | jq -r '.Id')
-          echo "printing change set ARN: $ARN"
+#           echo "creating change set for new stack"
+#           OUT=$(aws cloudformation create-change-set     --stack-name $StackName     --change-set-name my-change-set  \
+#               --template-body file://dynamo-cf-template.yaml --region $Region  --parameters ParameterKey=PrimaryKeyName,ParameterValue=$PrimaryKeyName   \
+#               ParameterKey=PrimaryKeyType,ParameterValue=$PrimaryKeyType ParameterKey=EnvironmentName,ParameterValue=$EnvironmentName ParameterKey=Region,ParameterValue=$Region --change-set-type CREATE)
+#           ARN=$(echo $OUT | jq -r '.Id')
+#           echo "printing change set ARN: $ARN"
 
         
-fi 
+# fi 
+
+aws cloudformation deploy --stack-name $StackName --template-file dynamo-cf-template.yaml --region $Region --no-execute-changeset \
+--parameter-overrides PrimaryKeyName=$PrimaryKeyName PrimaryKeyType=$PrimaryKeyType EnvironmentName=$EnvironmentName Region=$Region
 
