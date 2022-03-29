3c3cc1e2e6e82e0ff86f168c4a32733d9209ae60
mmangalg <102246141+mmangalg@users.noreply.github.com>
2022-03-29 13:27:26 +0530

    Update create-change-set.sh

--- create-change-set.sh
+++ create-change-set.sh
@@ -26,8 +26,6 @@ else
           ARN=$(echo $OUT | jq -r '.Id')
           echo "printing change set ARN: $ARN"
 
-          // sleep 15
-          // aws cloudformation execute-change-set --change-set-name $ARN --region $Region
-
+        
 fi 
 
